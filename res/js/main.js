3510cd95d536d1fdb3078404efa3b5264644faf2
Naty <natalie.vankova@student.spsmb.cz>
2022-10-13 08:42:52 +0200

    first commit

---
+++ res/js/main.js
@@ -0,0 +1,42 @@
+const Shrek = document.getElementById("Shrek");
+const counter = document.getElementById("counter");
+const autoclicker = document.getElementById("autoclicker");
+const clickupgrade = document.getElementById("clickupgrade");
+
+let Shreks = 0;
+let autoclickerPrice = 100;
+let autoclickerIncrease = 0;
+
+let clickupgradePrice = 50;
+let clickupgradeIncrease = 1;
+
+// ()  => - arrow function
+Shreks.onclick = () => {
+    Shrek++;
+    counter.innerHTML = Shreks;
+
+}
+autoclicker.onclick = () => {
+    if( cookies >= autoclickerPrice) {
+        cookies-= autoclickerPrice;
+        counter.innerHTML = cookies;
+        autoclickerPrice *= 2;
+        autoclicker.innerHTML = `Buy autoclicker: ${autoclickerPrice}`; 
+         if(autoclickerIncrease == 0) {
+          setInterval(() => {
+             cookies += autoclickerIncrease;
+             counter.innerHTML = cookies;
+          }, 1000);  
+        }
+        autoclickerIncrease++;
+    }
+}
+clickupgrade.onclick = () => {
+    if( Shrek >= clickupgradePrice) {
+        Shrek-= clickupgradePrice;
+        counter.innerHTML = Shreks;}
+        clickupgradePrice *= 2;
+        clickupgrade.innerHTML = `Buy clickupgrade: ${clickupgradePrice}`; 
+        clickupgradeIncrease++;
+
+} 
